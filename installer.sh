71c1df42bfaaaa2e7a1c9f048ac012e84a9a92e7
James Phelps <jamesph2012@outlook.com>
2025-01-11 11:30:36 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,14 @@
+cd ~
+
+cd vapp
+python3 -m venv vapp
+source vapp/bin
+
 pip install discord.py discord asyncio psutil pyautogui paperclip google-generativeai zenity
 
 sudo apt install espeak
 sudo apt install ffmpeg
 sudo apt install zenity
+
+echo "Installation Complete"
+echo "You may now run the bot.py with 'python3 bot.py'."
